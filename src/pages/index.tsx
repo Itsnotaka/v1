7ab26ba37493e746c13f739d8d5ac2b372d30362
Aka <itsnotaka@Gmail.com>
2021-12-28 23:38:08 +0800

    fix landing page space error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import {useEffect, useState} from 'react';
+import ReactMarkdown from 'react-markdown';
 import Header from '../components/Header';
 import Typist from 'react-typist';
 
@@ -10,21 +11,28 @@ const Home: NextPage = () => {
 	return (
 		<>
 			<Header />
-			<div className="space-y-4 text-center">
-				<div className="flex space-x-3">
+
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				{/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+				<div className="max-w-2xl mx-auto">
+					<div className="space-y-4 text-center">
+						<div className="flex space-x-3"></div>
+						<h1 className="font-mit font-bold text-4xl">
+							Hello, I am daniel 👋
+						</h1>
+						<Typist className="relative text-lg font-medium top-2 underline">
+							Be passionate about your life, be interested in everything
+							surrounding you.
+						</Typist>
+						<p className=" text-left opacity-80 hover:opacity-100">
+							Alright let's do this one last time. My name is Min Chun Fu. I am
+							an ordinary ${age} year old student.
+							I was not bitten by a radioactive spider.
+
+						</p>
+					</div>
 				</div>
-				<h1 className="font-mit font-bold text-4xl">Hello, I am daniel 👋</h1>
-				<Typist className="relative text-lg font-medium top-2 underline">
-					Be passionate about your life, be interested in everything surrounding
-					you.
-				</Typist>
-				<p className="opacity-80">
-					Alright let's do this one last time. My name is Min Chun Fu. I am an
-					ordinary ${age} year old student. I was ~not~ bitten by a radio active
-					spider
-				</p>
 			</div>
-			<div className="relative text-center"></div>
 		</>
 	);
 };
