e7c1692db211645926469fc4aaaa03456b1d243d
Aka <itsnotaka@Gmail.com>
2022-01-01 10:10:43 +0800

    head for all pages

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Typist from 'react-typist';
 import ContactForm from '../../components/ContactForm';
 import Header from '../../components/_Header';
@@ -5,6 +6,9 @@ import Header from '../../components/_Header';
 const Chat = () => {
 	return (
 		<>
+			<Head>
+				<title>Chat - Daniel Fu</title>
+			</Head>
 			<Header />
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				{/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
@@ -14,9 +18,7 @@ const Chat = () => {
 						<Typist className="font-mit font-bold text-4xl">
 							Let's Chat...💬
 						</Typist>
-						<h3 className="text-lg font-bold">
-							Feel free to ask me anything!
-						</h3>
+						<h3 className="text-lg font-bold">Feel free to ask me anything!</h3>
 						<ContactForm />
 					</div>
 				</div>
